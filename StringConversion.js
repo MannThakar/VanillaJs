172b8b14fe933745631957f5b25743e95fefb2f7
Mannthakar24 <210540107514@darshan.ac.in>
2023-10-22 10:43:35 +0530

    Conversion

--- StringConversion.js
+++ StringConversion.js
@@ -1,7 +1,10 @@
 "use strict"
 
+// console.log(1 + "1" + 1)
+// console.log(1 + 1 + "1")
+// console.log("1" + 1 + 1)
 
-console.log(1 + "1");
-console.log("1" + 1);
-console.log("1" + 1 + 1);
-console.log(1 + "1");
+let A =  1 + "1";
+
+
+console.log(typeof A);
