bdbd5714285c688741ab92d8b7aabac4fb19f03d
Mannthakar24 <210540107514@darshan.ac.in>
2023-10-22 11:42:12 +0530

    Comparisons

---
+++ Comparition.js
@@ -0,0 +1,13 @@
+
+//Null Conversion
+console.log(null >= 0)
+console.log(null <= 0)
+console.log(null == 0)
+
+//Undefined Conversion
+console.log(undefined >= 0)
+console.log(undefined == 0)
+console.log(undefined <= 0)
+
+//Null and Undefined
+console.log(null==undefined)
--- StringConversion.js
+++ StringConversion.js
@@ -1,10 +1,5 @@
 "use strict"
 
-// console.log(1 + "1" + 1)
-// console.log(1 + 1 + "1")
-// console.log("1" + 1 + 1)
-
-let A =  1 + "1";
-
-
-console.log(typeof A);
+console.log(1 + "1" + 1)
+console.log(1 + 1 + "1")
+console.log("1" + 1 + 1)
