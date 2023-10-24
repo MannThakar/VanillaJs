0df64d87fdcf2ac73f08c0b4347fd4cc52dba8bb
Mannthakar24 <210540107514@darshan.ac.in>
2023-10-24 15:18:28 +0530

    Primitive and Non Primitive

---
+++ DataType-summary.js
@@ -0,0 +1,24 @@
+"use strict"
+
+//Primitive Datatypes
+
+let bigNumber = 12345678n;
+
+
+//Non Primitive DataTypes
+
+let Arr = [1, 2, 4, 5, 6];
+
+console.log(typeof Arr)
+
+//Function
+let Fun1 = function Add() {
+    console.log("Hello");
+}
+
+//Objects
+const Man = {
+    height: 5.9,
+    weight: 64
+
+}
