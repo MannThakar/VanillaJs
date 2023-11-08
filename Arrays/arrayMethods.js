2f353a50e45a3372bed308fb8fc192b773a3f98a
Mannthakar24 <210540107514@darshan.ac.in>
2023-11-08 21:12:30 +0530

    Array's Methods

--- Arrays/arrayMethods.js
+++ Arrays/arrayMethods.js
@@ -1,6 +1,6 @@
 "use strict"
 
-const arr = [1, 2, 3, 4, 5, 7];
+// const arr = [1, 2, 3, 4, 5, 7];
 
 //Add item at begining of array
 // console.log(arr.unshift(8));
@@ -11,7 +11,7 @@ const arr = [1, 2, 3, 4, 5, 7];
 // console.log(arr);
 
 //Return true if value exist
-console.log(arr.includes(5));
+// console.log(arr.includes(5));
 
 
 const actor = [
@@ -32,8 +32,80 @@ const actor = [
     },
 ];
 
+//Generic For Loop
+// for (let i = 0; i < actor.length; i++) {
+//     actor[i].fees -= 50;
+// }
 
-for (let i = 0; i < actor.length; i++) {
-    actor[i].fees -= 50;
-}
+//For each loop
+
+const newArray = actor.map((e) => e.name);
+
+// Add a debug statement to check the `actor` array
+
+
+// const strArray = newArray.join('\n');
+// console.log(strArray);
+
+
+// const index = actor.map((e, i) => e.fees -= 50)
+
+
+
+const arr = ["Apple", "Banana", "Mango", "Pineapple"];
+
+
+// const sliceArray = arr.slice(0, 2);
+// console.log(sliceArray);
+
+// arr.forEach(function run(e, i) {
+//     console.log(`${e}:- ${i}`);
+// })
+
+
+// const element = arr.map((e, i) => {
+//     return e + e
+// });
+// console.log(element);
+
+
+// arr.push("Apple");
+// arr.push("Apple");
+// arr.push("Banana");
+
+// console.log(arr);
+
+// const NewFilterArray = arr.filter((e) => e === "Apple");
+
+// console.log(NewFilterArray);
+
+
+// const newArr = arr.concat(['Resberry','Orange','WaterMelon']);
+
+// console.log(newArr);
+
+
+// const Found = arr.find((e) => e === "Apdple")
+
+// console.log(Found);
+
+// const hasAnApple = arr.some(() => "apple");
+
+// console.log(hasAnApple);
+
+
+// const Headphones = ["Realme","Zeb thunder","Read Gear"];
+// const newCollection = Headphones.splice(2,1);
+// console.log(newCollection);
+
+
+// console.log(Array.isArray(actor));
+
+// const Itrator = actor.entries();
+
+// for(const It of Itrator)
+// {
+//     console.log(It);
+// }
+
 
