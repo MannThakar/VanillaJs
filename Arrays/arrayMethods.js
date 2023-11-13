a2d7e468ffb8232fd194fcb88b32fb4b2b83bca9
Mannthakar24 <210540107514@darshan.ac.in>
2023-11-13 11:35:26 +0530

    This files are added

--- Arrays/arrayMethods.js
+++ Arrays/arrayMethods.js
@@ -109,3 +109,11 @@ const arr = ["Apple", "Banana", "Mango", "Pineapple"];
 // }
 
 
+// const arr1 = [1,2,3,4];
+// const arr2 = [5,6,7,8];
+
+
+// const arr3=arr2.concat(arr1)
+
+// arr3.sort()
+// console.log(arr3);
---
+++ objects/basicOfObject.js
@@ -0,0 +1,29 @@
+
+"use strict"
+
+//Object Declared
+const obj = {
+    name: "Mann Thakar",
+    age: 21,
+    college: "Darshan University",
+    language: ["Gujarati", "Hindi", "English"]
+};
+
+
+//Oneway to access the property of object
+console.log(obj.age);
+
+//Second way to access the propery of object
+console.log(obj["age"]);
+
+
+//Frezzing the object
+Object.freeze(obj);
+obj['age'] = 22;
+
+
+//Using This function
+obj.myFun = function () { 
+    console.log(`Hello Happy Diwali to ${this.name}`);
+};
+
